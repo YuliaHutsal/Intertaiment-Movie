b84a7802efd9358cacaf809eb1cf7cb6620d09fe
Yulia Gutsal <julik.ua@gmail.com>
2023-01-12 22:14:59 +0200

    footerbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/Header/Header.js';
+import SimpleBottomNavigation from './components/MainNav';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
-      {/* <Router>
-        <Header />
-          <Routes>
-            <Route index element={<h1>Movie App</h1>}></Route>
-            <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
-            <Route path="movies/:type" element={<h1>Movies list page</h1>}></Route>
-            <Route path="/*" element={<h1>Error Page</h1>}></Route>
-          </Routes>
-      </Router> */}
-    </div>
+      <div className="App">Context </div>
+      <SimpleBottomNavigation />
+    </>
   );
 }
 
---
+++ src/components/MainNav.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './MainNav.css';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import MovieIcon from '@mui/icons-material/Movie';
+import SearchIcon from '@mui/icons-material/Search';
+import TvIcon from '@mui/icons-material/Tv';
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Trending" icon={<WhatshotIcon />} />
+        <BottomNavigationAction label="Movies" icon={<MovieIcon />} />
+        <BottomNavigationAction label="TV series" icon={<TvIcon />} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
