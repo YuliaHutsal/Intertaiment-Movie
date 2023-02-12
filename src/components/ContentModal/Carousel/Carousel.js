abfb521e3fba9af7473c7cc29f67338b1e73fac5
Yulia Gutsal <julik.ua@gmail.com>
2023-02-12 16:45:27 +0200

    carousel.css improve

--- src/components/ContentModal/Carousel/Carousel.js
+++ src/components/ContentModal/Carousel/Carousel.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-
 import { img_300, noPicture } from '../../../config/config';
+import './Carousel.css';
 
 const handleDragStart = (e) => e.preventDefault();
 
 
-const Carousel = (media_type, id) => {
-  const [credits, setCredits] = useState();
+const Carousel = ({media_type, id}) => {
+  const [credits, setCredits] = useState([]);
 
- const items = credits?.map((c) => (
+ const items = credits.map((c) => (
 <div className='carouselItem'>
   <img
   src={c.profile_path ? `${img_300}/${c.profile_path}` : noPicture}
@@ -22,6 +22,18 @@ const Carousel = (media_type, id) => {
   <b className='carouselItem_txt'>{c?.name}</b>
 </div>
  ));
+
+ const responsive = {
+  0:{
+    items: 3,
+  },
+  512:{
+    items:5,
+  },
+  1024:{
+    items:7,
+  },
+ };
 
   const fetchCredits = async() => {
     const {data} = await axios.get(
@@ -35,7 +47,15 @@ const Carousel = (media_type, id) => {
   },[])
 
   return (
-    <AliceCarousel mouseTracking items={items} />
+    <AliceCarousel 
+    mouseTracking 
+    items={items}
+    responsive={responsive}
+    infinite
+    disableDotsControls
+    disableButtonsControls
+    autoPlay
+    />
   );
 };
 export default Carousel;
